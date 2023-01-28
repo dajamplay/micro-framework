be61811729087eabd61bb3df51af1a78f1d7b856
Maksim Aksenov <ggyoug@gmail.com>
2023-01-28 12:35:44 +0300

    add sample db file

---
+++ config/database-sample.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * WARNING!!!
+ * Rename this file to "database.php"
+ */
+
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => '',
+    'username' => '',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
